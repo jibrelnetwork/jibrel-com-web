811cb1a1be8456dad1d7e1bed34c8414bad0bab3
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-11-05 23:52:07 +0300

    Add offering page

--- api/pages/controllers/Pages.js
+++ api/pages/controllers/Pages.js
@@ -8,6 +8,18 @@ const { sanitizeEntity } = require('strapi-utils')
  * to customize this controller
  */
 
+function transformOffering(offering) {
+  const o = sanitizeEntity(
+    offering,
+    { model: strapi.models.primaryoffering }
+  )
+  o.preview_bg_img = _.get(o, 'preview_bg_img.url', '#')
+  o.logo_img = _.get(o, 'logo_img.url', '#')
+  o.url = `/${o.path}`
+
+  return o
+}
+
 module.exports = {
   async list(ctx) {
     const lang = ctx.getLocaleFromUrl()
@@ -21,22 +33,31 @@ module.exports = {
     })
 
     const offerings = rawOfferings
-      .map((offering) => {
-        const o = sanitizeEntity(
-          offering,
-          { model: strapi.models.primaryoffering }
-        )
-        o.preview_bg_img = _.get(o, 'preview_bg_img.url', '#')
-        o.logo_img = _.get(o, 'logo_img.url', '#')
-        o.url = `/${o.path}`
-
-        return o
-      })
+      .map(transformOffering)
 
     await ctx.render('index.hbs', {
       lang,
       dir,
       offerings,
     })
-  }
+  },
+
+  async offering(ctx) {
+    const lang = ctx.getLocaleFromUrl()
+    const dir = lang === 'ar'
+      ? 'rtl'
+      : 'ltr'
+
+    const rawOffering = await strapi.query('primaryoffering').findOne({
+      active: true,
+      lang,
+      slug: ctx.params.slug,
+    })
+
+    await ctx.render('primary-offering.hbs', {
+      ...transformOffering(rawOffering),
+      lang,
+      dir,
+    })
+  },
 }
