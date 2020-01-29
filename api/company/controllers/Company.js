61c0aafaeeefb774fd9b35f28cd1a9c9373dd1e1
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2020-01-29 18:21:45 +0300

    Add Company.slug service

--- api/company/controllers/Company.js
+++ api/company/controllers/Company.js
@@ -5,4 +5,34 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { rgbaToHex } = require('utils/color')
+
+module.exports = {
+  async slug(ctx) {
+    const data = await strapi.services.company.slug(ctx.params.slug)
+
+    if (!data) {
+      return ctx.notFound()
+    }
+
+    const primaryColor = data.primary_color
+
+    const primaryColorRGB = [
+      primaryColor.slice(1, 3),
+      primaryColor.slice(3, 5),
+      primaryColor.slice(5, 7),
+    ].map((hex) => parseInt(hex, 16))
+
+    return {
+      data: {
+        slug: data.slug,
+        title: data.title,
+        color: {
+          primary: data.primary_color,
+          background: rgbaToHex([...primaryColorRGB, 0.1]),
+        },
+        logo: data.logo_img.url,
+      },
+    }
+  },
+}
