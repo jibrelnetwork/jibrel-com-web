25d02011f22b77fbb1dd44b0e1c8c22a0d7289e1
Ivan Violentov <violentoff@gmail.com>
2020-02-26 15:22:10 +0300

    Add /api/v1/companies endpoint

--- api/company/controllers/Company.js
+++ api/company/controllers/Company.js
@@ -10,7 +10,54 @@ const {
   hexToRgb,
 } = require('utils/color')
 
+function getCompanyData(company) {
+  const {
+    slug,
+    title,
+    location,
+    permalink,
+    translation,
+    hero_img: hero,
+    logo_img: logo,
+    primary_color: primary,
+    current_offering: currentOffering,
+  } = company
+
+  const primaryColorRGB = hexToRgb(primary || '#003dc6')
+
+  return {
+    data: {
+      slug,
+      title,
+      location,
+      permalink,
+      currentOffering,
+      tagline: translation.tagline,
+      color: {
+        primary,
+        background: rgbaToHex([...primaryColorRGB, 0.1]),
+      },
+      hero: hero
+        ? hero.url
+        : null,
+      logo: logo
+        ? logo.url
+        : null,
+    },
+  }
+}
+
 module.exports = {
+  async list(ctx) {
+    const raw = await strapi.services.company.list()
+
+    return raw.map((item) => {
+      const data = strapi.services.company.localize(ctx.i18n, item)
+
+      return getCompanyData(data)
+    })
+  },
+
   async slug(ctx) {
     const raw = await strapi.services.company.slug(ctx.params.slug)
     const data = strapi.services.company.localize(ctx.i18n, raw)
@@ -19,39 +66,6 @@ module.exports = {
       return ctx.notFound()
     }
 
-    const {
-      slug,
-      title,
-      location,
-      permalink,
-      translation,
-      hero_img: hero,
-      logo_img: logo,
-      primary_color: primary,
-      current_offering: currentOffering,
-    } = data
-
-    const primaryColorRGB = hexToRgb(primary || '#003dc6')
-
-    return {
-      data: {
-        slug,
-        title,
-        location,
-        permalink,
-        currentOffering,
-        tagline: translation.tagline,
-        color: {
-          primary,
-          background: rgbaToHex([...primaryColorRGB, 0.1]),
-        },
-        hero: hero
-          ? hero.url
-          : null,
-        logo: logo
-          ? logo.url
-          : null,
-      },
-    }
+    return getCompanyData(data)
   },
 }
