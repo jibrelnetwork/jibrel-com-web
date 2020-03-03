83488b5d3dd3527ca051f9f6cae7e2a9e2e5e748
Ivan Violentov <violentoff@gmail.com>
2020-03-03 14:01:57 +0300

    Add redirecting for pages without lang

--- api/pages/config/policies/withValidLanguage.js
+++ api/pages/config/policies/withValidLanguage.js
@@ -1,20 +1,15 @@
-const DEFAULT_LANGUAGE = 'en'
-const LANGUAGES_AVAILABLE = {
-  en: true
-}
-
 module.exports = async (ctx, next) => {
   const { i18n } = ctx
   const { lang } = ctx.params
 
   if (!lang) {
     // FIXME: should instead use ctx.i18n.locale filled also from cookie
-    return ctx.redirect(`/${i18n.defaultLocale}`)
+    return ctx.redirect(`/${i18n.defaultLocale}${ctx.originalUrl}`)
   }
 
   if (!i18n.locales[lang]) {
     // FIXME: should be not found?
-    return ctx.redirect(`/${i18n.defaultLocale}`)
+    return ctx.redirect(`/${i18n.defaultLocale}${ctx.originalUrl}`)
   }
 
   return next()
