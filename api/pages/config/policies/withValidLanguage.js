e6b826d9e545d7117db60681494f2f0df5202893
Ivan Violentov <violentoff@gmail.com>
2020-03-03 17:52:54 +0300

    Fix routes redirecting

--- api/pages/config/policies/withValidLanguage.js
+++ api/pages/config/policies/withValidLanguage.js
@@ -1,15 +1,26 @@
+const URL_FIRST_SLASH_POS = 1
+
 module.exports = async (ctx, next) => {
-  const { i18n } = ctx
-  const { lang } = ctx.params
+  const {
+    i18n,
+    params,
+    originalUrl,
+  } = ctx
+
+  const { lang } = params
 
   if (!lang) {
     // FIXME: should instead use ctx.i18n.locale filled also from cookie
-    return ctx.redirect(`/${i18n.defaultLocale}${ctx.originalUrl}`)
+    return ctx.redirect(`/${i18n.defaultLocale}${originalUrl}`)
   }
 
   if (!i18n.locales[lang]) {
     // FIXME: should be not found?
-    return ctx.redirect(`/${i18n.defaultLocale}${ctx.originalUrl}`)
+
+    const langSlashPos = originalUrl.indexOf('/', URL_FIRST_SLASH_POS)
+    const urlWithoutLang = (langSlashPos !== -1) ? originalUrl.substr(langSlashPos) : ''
+
+    return ctx.redirect(`/${i18n.defaultLocale}${urlWithoutLang}`)
   }
 
   return next()
